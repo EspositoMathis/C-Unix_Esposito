df4858e72352b2051c7166d17aa582a462da6a1f
EspositoMathis <espositm@3il.fr>
2025-12-17 15:10:44 +0100

    PAUSE

---
+++ calcul_parallele.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define TAILLE_DONNEES 10
+#define MULTIPLICATEUR 1000
+
+static const int DONNEES[TAILLE_DONNEES] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+typedef struct {
+    int valeur;
+    int multiplicateur;
+    int index;
+} args_t;
+
+void* calcul_thread(void* arg) {
+        // cette fonction est appelé via pthread_create
+        // vous savez ce que représente @arg
+        // vous notifié au compilateur "cette" address dois être considéré comme args_t
+    args_t* args = (args_t*)arg;
+        // vous pouvez accéder (read/write) a toutes les variables via `args->`
+
+    return NULL;
+}
+
+
+int main(void) {
+   printf("=== Calcul Parallèle ===\n");
+
+    struct timespec debut, fin;
+    clock_gettime(CLOCK_MONOTONIC, &debut);
+
+    // TODO: Créer un tableau de pthread_t
+    pthread_t tab[TAILLE_DONNEES];
+    // TODO: Créer un tableau de structures d'arguments
+    args_t args[TAILLE_DONNEES];
+
+    // TODO: Créer et démarrer tous les threads
+    for (int i=0; i<TAILLE_DONNEES; i++){
+        
+        args[i].index = DONNEES[i];
+        args[i].multiplicateur = MULTIPLICATEUR;
+        args[i].index = i;
+
+        pthread_create(tab[i], NULL, calcul_thread, (void*)args[i]);
+    }
+
+    // TODO : Créer tous les threads
+    // TODO: Attendre tous les threads
+    int result[TAILLE_DONNEES];
+    int* r;
+    for (int i=0; i<TAILLE_DONNEES; i++){
+        pthread_join(tab[i], (void*)r);
+        result[i] = *r ;
+    }
+    // TODO: Agréger les résultats
+    for (int i=0; i<TAILLE_DONNEES; i++){
+        
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &fin);
+    long duree = (fin.tv_sec - debut.tv_sec) * 1000 + 
+                 (fin.tv_nsec - debut.tv_nsec) / 1000000;
+    long int somme_total = 0;
+    printf("Résultat total : %ld\n", somme_total);
+    printf("Durée : %ld ms\n", duree);
+
+    return 0;
+}
